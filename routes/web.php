37b3580938bf168c87bd072653ebce43417b0eea
mateussousadev <sousamateus443@gmail.com>
2025-12-17 01:12:06 -0300

    feat: inicia votacao sem autenticação

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\VoteController;
 use App\Models\Award;
+use App\Models\Vote;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
@@ -15,10 +18,26 @@ Route::get('/', function () {
 });
 
 Route::get('/voting/{award_id}', function ($award_id) {
-
     $award = Award::where('id', $award_id)
         ->where('is_active', true)
-        ->with('categories.nominees')
+        ->with(['categories.nominees'])
         ->firstOrFail();
-    return view('votingSession', compact('award'));
+
+    $userVotes = [];
+    if (Auth::check()) {
+        $userVotes = Vote::where('user_id', Auth::id())
+            ->whereIn('category_id', $award->categories->pluck('id'))
+            ->get()
+            ->keyBy('category_id');
+    }
+
+    return view('votingSession', compact('award', 'userVotes'));
 })->name('votingSession');
+
+Route::post('/voting/{award_id}/vote', [VoteController::class, 'store'])
+    ->middleware(['auth', 'throttle:10,1'])
+    ->name('vote.store');
+
+Route::delete('/voting/{award_id}/vote', [VoteController::class, 'destroy'])
+    ->middleware(['auth', 'throttle:10,1'])
+    ->name('vote.destroy');
