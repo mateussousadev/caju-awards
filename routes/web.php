43749b69eea6b7e8d4edd5231acca3f99c76f907
mateussousadev <sousamateus443@gmail.com>
2025-12-17 00:08:14 -0300

    feat: lista as premiacoes na home

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,24 @@
 <?php
 
+use App\Models\Award;
 use Illuminate\Support\Facades\Route;
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
+
+    $dados = Award::where('is_active', true)
+        ->get();
+    return view('home', compact('dados'));
 });
+
+Route::get('/voting/{award_id}', function ($award_id) {
+
+    $award = Award::where('id', $award_id)
+        ->where('is_active', true)
+        ->with('categories.nominees')
+        ->firstOrFail();
+    return view('votingSession', compact('award'));
+})->name('votingSession');
