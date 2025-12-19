c5f0aa7677f2c8203034e6f0b49cf78156121058
RegisJrdev <regisjrdev@gmail.com>
2025-12-19 00:17:53 -0300

    fix: Adiciona favicons em todas as pages

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="icon" type="image/x-icon" href="{{ asset('img/favicon.ico') }}">
+
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="bg-gray-950 min-h-screen flex items-center justify-center p-4">
